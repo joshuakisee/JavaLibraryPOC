ef092e2e5eb482e5140b9a6b1099fa16b4d69d64
Joshua Kisee <you@example.com>
2022-11-19 14:39:06 +0300

    first commit

---
+++ src/main/java/com/cellulant/librarydemo/PostPaymentService.java
@@ -0,0 +1,18 @@
+package com.cellulant.librarydemo;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author joshua.kisee
+ * @project LibraryDemo
+ * @created 19/11/2022
+ */
+
+@Service
+public class PostPaymentService {
+
+    public String testing(){
+        return "a library working";
+    }
+
+}
